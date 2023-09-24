24eda1bace238d20f80de3a13651e28a95bc5e7c
patrykluszcz <patis00@wp.pl>
2023-09-24 22:04:19 +0200

    feat: add new poppins font + change prefix of component selector (website)

--- frontend/apps/website/src/app/app.component.ts
+++ frontend/apps/website/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'frontend-root',
+  selector: 'web-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
