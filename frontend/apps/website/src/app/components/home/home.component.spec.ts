2702b6fc7cf834519cac34aaa33e24807bc88e75
patrykluszcz <patis00@wp.pl>
2023-09-26 01:00:59 +0200

    ref: change name of imported header component into home

--- frontend/apps/website/src/app/components/home/home.component.spec.ts
+++ frontend/apps/website/src/app/components/home/home.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HeaderComponent } from './home.component';
+import { HomeComponent } from './home.component';
 
 describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [HeaderComponent],
+      declarations: [HomeComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
