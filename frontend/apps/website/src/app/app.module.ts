e4c904fe413c0504cc427af6545ee51f1b88b25e
patrykluszcz <patis00@wp.pl>
2023-09-26 00:41:27 +0200

    ref: replace header component name by home

--- frontend/apps/website/src/app/app.module.ts
+++ frontend/apps/website/src/app/app.module.ts
@@ -4,10 +4,10 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NavigationComponent } from './components/navigation/navigation.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent, HeaderComponent],
+  declarations: [AppComponent, NavigationComponent, HomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
