24baba5351da20227559d5edabe85a7e8d3c493b
patrykluszcz <patis00@wp.pl>
2023-10-01 22:47:34 +0200

    feat: create shared module and import it into app module

--- frontend/apps/manage-pro/src/app/app.module.ts
+++ frontend/apps/manage-pro/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
+import { SharedModule } from './shared.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    SharedModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
