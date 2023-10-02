2ac4b745bda862b6d9208a70239d33ff93b3a11c
patrykluszcz <patis00@wp.pl>
2023-10-02 20:15:26 +0200

    feat: create authorization service

---
+++ frontend/apps/manage-pro/src/app/shared/services/authorization.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthorizationService {
+  private _baseUrl = 'http://localhost:8080';
+
+  constructor(private _http: HttpClient) {}
+
+  public postAuthorizationLogin(login: any): Observable<any> {
+    return this._http.post(`${this._baseUrl}/login`, login);
+  }
+}
