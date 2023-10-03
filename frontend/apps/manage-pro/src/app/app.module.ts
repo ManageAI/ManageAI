7e22038667c9fb27d043f7dfe2c176d7598c13ca
patrykluszcz <patis00@wp.pl>
2023-10-03 17:08:12 +0200

    fix: import HttpClientModule into app module

--- frontend/apps/manage-pro/src/app/app.module.ts
+++ frontend/apps/manage-pro/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { SharedModule } from './shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule, RouterModule.forRoot(appRoutes)],
+  imports: [
+    BrowserModule,
+    SharedModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
