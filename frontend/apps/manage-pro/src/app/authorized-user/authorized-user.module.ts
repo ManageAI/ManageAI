dff7efce180ea4ffdb20f4ff6a0213c94dbcecde
patrykluszcz <patis00@wp.pl>
2023-10-22 19:44:28 +0200

    feat: implementation of authorization logic

--- frontend/apps/manage-pro/src/app/authorized-user/authorized-user.module.ts
+++ frontend/apps/manage-pro/src/app/authorized-user/authorized-user.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthorizedUserRoutingModule } from './authorized-user-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AuthorizedUserComponent } from './authorized-user.component';
 
 @NgModule({
   declarations: [AuthorizedUserComponent],
-  imports: [CommonModule, AuthorizedUserRoutingModule],
-})
+  imports: [
+    CommonModule,
+    AuthorizedUserRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],})
 export class AuthorizedUserModule {}
--- frontend/apps/manage-pro/src/app/shared.module.ts
+++ frontend/apps/manage-pro/src/app/shared.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule],
   exports: [],
 })
 export class SharedModule {}
