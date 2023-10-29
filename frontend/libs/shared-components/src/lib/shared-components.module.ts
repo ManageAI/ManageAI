8d50da3c85fd317bc9bf456c3def29acb24fbb23
patrykluszcz <patis00@wp.pl>
2023-10-29 19:07:45 +0100

    feat: generate input shared component

--- frontend/libs/shared-components/src/lib/shared-components.module.ts
+++ frontend/libs/shared-components/src/lib/shared-components.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+
 import { ButtonComponent } from './components/button/button.component';
+import { InputComponent } from './components/input/input.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [ButtonComponent],
+  imports: [CommonModule, MatInputModule],
+  declarations: [ButtonComponent, InputComponent],
   exports: [ButtonComponent],
 })
 export class SharedComponentsModule {}
