c9af83d44b6aaf59aa53de25d8e37f50df27091b
patrykluszcz <patis00@wp.pl>
2023-11-02 19:24:13 +0100

    feat: make reusable input component

--- frontend/libs/shared-components/src/lib/components/input/input.component.ts
+++ frontend/libs/shared-components/src/lib/components/input/input.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'shared-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss'],
 })
-export class InputComponent {}
+export class InputComponent {
+  @Input() id!: string;
+  @Input() label!: string;
+  @Input() type!: string;
+  @Input() placeholder!: string;
+  @Input() iconSrc!: string;
+}
--- frontend/libs/shared-components/src/lib/shared-components.module.ts
+++ frontend/libs/shared-components/src/lib/shared-components.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ButtonComponent } from './components/button/button.component';
 import { InputComponent } from './components/input/input.component';
 
 @NgModule({
-  imports: [CommonModule, BrowserAnimationsModule],
+  imports: [CommonModule],
   declarations: [ButtonComponent, InputComponent],
-  exports: [ButtonComponent],
+  exports: [ButtonComponent, InputComponent],
 })
 export class SharedComponentsModule {}
