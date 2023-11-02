ecb5e5f54c01229a505fa2d4fea1c1075232bfac
patrykluszcz <patis00@wp.pl>
2023-11-02 21:46:13 +0100

    feat: implement reusable logic for input component and provide value accessor to manage custom value forms

--- frontend/libs/shared-components/src/lib/components/input/input.component.ts
+++ frontend/libs/shared-components/src/lib/components/input/input.component.ts
@@ -1,14 +1,68 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  forwardRef,
+} from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'shared-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
   @Input() id!: string;
   @Input() label!: string;
   @Input() type!: string;
   @Input() placeholder!: string;
   @Input() iconSrc!: string;
+  @Input() isReadOnly!: boolean;
+
+  @Output() valueChange = new EventEmitter<string>();
+
+  onChange!: <T>(value: T) => void;
+  onTouched!: () => void;
+
+  isDisabled!: boolean;
+  value!: unknown;
+
+  // NG_VALUE_ACCESSOR interface methods
+
+  writeValue<T>(value: T): void {
+    if (!value) {
+      this.value = null;
+      return;
+    }
+
+    this.value = value;
+  }
+
+  registerOnChange(fn: () => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+
+  setInput(target: EventTarget | null): void {
+    const inputValue = (target as HTMLInputElement).value;
+
+    this.value = inputValue?.trim();
+    this.valueChange.emit(inputValue);
+    this.onChange(inputValue);
+  }
 }
