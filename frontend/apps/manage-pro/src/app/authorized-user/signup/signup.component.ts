9914132e7b071826aeb2fc2d7731d84524e034f4
patrykluszcz <patis00@wp.pl>
2023-11-04 21:56:39 +0100

    feat: create signup component

---
+++ frontend/apps/manage-pro/src/app/authorized-user/signup/signup.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss'],
+})
+export class SignupComponent {
+  iconUser = 'assets/icons/user.svg';
+  iconEmail = 'assets/icons/email.svg';
+  iconPassword = 'assets/icons/password.svg';
+  iconLoop = 'assets/icons/loop.svg';
+
+  signUpForm = this._formBuilder.nonNullable.group({
+    fullName: ['', [Validators.required, Validators.email]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  constructor(private _formBuilder: FormBuilder) {}
+}
