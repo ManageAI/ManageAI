7b0e9b5e4c51d204227018bc2ab44ba0f0e4c780
patrykluszcz <patis00@wp.pl>
2023-11-04 21:56:11 +0100

    feat: create login component

---
+++ frontend/apps/manage-pro/src/app/authorized-user/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+  iconEmail = 'assets/icons/email.svg';
+  iconPassword = 'assets/icons/password.svg';
+
+  loginForm = this._formBuilder.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  constructor(private _formBuilder: FormBuilder) {}
+}
