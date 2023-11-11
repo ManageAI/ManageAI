e38a53aa21ae494719efa0c768988d960a3961c0
KokoraMaksym <critticaleror@gmail.com>
2023-11-11 14:28:06 +0100

    fix: add @Cors annotation to allow front-ent communicate with back-end

--- backend/manage-pro-back-end/src/main/java/com/example/managepro/controller/AuthController.java
+++ backend/manage-pro-back-end/src/main/java/com/example/managepro/controller/AuthController.java
@@ -7,6 +7,7 @@ import com.example.managepro.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin()
 public class AuthController {
     private final AuthService authService;
 
