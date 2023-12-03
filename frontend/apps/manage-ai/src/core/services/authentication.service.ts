917983e1fdfe0eb24b3a3cbd988fc0ff3b4a483e
patrykluszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2023-12-03 12:27:34 +0100

    feat: implement registration endpoint in authentication service

--- frontend/apps/manage-ai/src/core/services/authentication.service.ts
+++ frontend/apps/manage-ai/src/core/services/authentication.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Authentication } from '../interfaces/authentication.interface';
 import { environment } from '../../environments/environment';
+import { Registration } from '../interfaces/registration.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -14,5 +15,9 @@ export class AuthenticationService {
 
   public authenticationLogin(login: Partial<Authentication>): Observable<any> {
     return this._http.post(`${this._baseUrl}/login`, login);
+  }
+
+  public registration(signUp: Partial<Registration>): Observable<any> {
+    return this._http.post(`${this._baseUrl}/signup`, signUp);
   }
 }
