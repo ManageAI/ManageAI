c31a3992a6d1d01b9b67122542716cd04516feaa
patrykluszcz <patis00@wp.pl>
2024-01-12 01:11:07 +0100

    ref: remove unnecessary NgModule classes and bootstrap the project using standalone APIs

--- frontend/apps/manage-ai/src/main.ts
+++ frontend/apps/manage-ai/src/main.ts
@@ -1,6 +1,18 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { appRoutes } from './app/app.routes';
+import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient } from "@angular/common/http";
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule),
+        provideAnimations(),
+        provideRouter(appRoutes),
+        provideHttpClient()
+    ]
+})
   .catch((err) => console.error(err));
