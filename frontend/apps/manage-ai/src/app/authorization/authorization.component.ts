055cd7f4ba140e48b35d98fccde7fd144ff55bbf
patrykluszcz <patis00@wp.pl>
2024-01-28 20:51:00 +0100

    feat: add router-outlet to authorization component

--- frontend/apps/manage-ai/src/app/authorization/authorization.component.ts
+++ frontend/apps/manage-ai/src/app/authorization/authorization.component.ts
@@ -1,11 +1,13 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-authorization',
-    templateUrl: './authorization.component.html',
-    styleUrls: ['./authorization.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: true,
+  selector: 'app-authorization',
+  templateUrl: './authorization.component.html',
+  styleUrls: ['./authorization.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [RouterOutlet],
 })
 export class AuthorizationComponent implements OnInit {
   public ngOnInit(): void {
