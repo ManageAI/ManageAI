a466ccd3618b2e6894eb6104a75c8f405dfc3ed4
patrykluszcz <patis00@wp.pl>
2024-02-05 01:19:06 +0100

    feat: implement authentication logic + set token in cookie and user data in local storage

--- frontend/apps/manage-ai/src/core/services/authentication.service.ts
+++ frontend/apps/manage-ai/src/core/services/authentication.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, map, take } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { Authentication } from '../interfaces/authentication.interface';
 import { environment } from '../../environments/environment';
-import { Registration } from '../interfaces/registration.interface';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthorizedUser } from '../interfaces/authorized-user.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -11,13 +11,48 @@ import { Registration } from '../interfaces/registration.interface';
 export class AuthenticationService {
   private _baseUrl = environment.url;
 
-  constructor(private _http: HttpClient) {}
+  private _authorizedUser = new BehaviorSubject<AuthorizedUser>(
+    JSON.parse(localStorage.getItem('authorizedUser') || '{}')
+  );
+  authorizedUser$ = this._authorizedUser.asObservable();
 
-  public authenticationLogin(login: Partial<Authentication>): Observable<any> {
-    return this._http.post(`${this._baseUrl}/login`, login);
+  constructor(private _http: HttpClient, private _cookieService: CookieService) {}
+
+  public authentication<T>(endpoint: string, form: T): Observable<string[]> {
+    return this._http.post<AuthorizedUser>(`${this._baseUrl}/${endpoint}`, form).pipe(
+      take(1),
+      map((user) => {
+        this._handleUserAuthentication(user);
+
+        return user.userRoles;
+      })
+    );
   }
 
-  public registration(signUpForm: Registration): Observable<any> {
-    return this._http.post(`${this._baseUrl}/signup`, signUpForm);
+  private _handleUserAuthentication(user: AuthorizedUser): void {
+    this._setTokenInCookie(user.token);
+    this._setUserDataInStorage(user);
+
+    this._authorizedUser.next(user);
+  }
+
+  private _setTokenInCookie(token: string): void {
+    this._cookieService.set('manage-ai-token', token, { path: '/', secure: true });
+  }
+
+  private _setUserDataInStorage(user: AuthorizedUser): void {
+    const userData = this._pickUserData(user);
+    localStorage.setItem('authorizedUser', JSON.stringify(userData));
+  }
+
+  private _pickUserData(
+    user: AuthorizedUser
+  ): Pick<AuthorizedUser, 'id' | 'fullName' | 'email' | 'userRoles'> {
+    return {
+      id: user.id,
+      fullName: user.fullName,
+      email: user.email,
+      userRoles: user.userRoles,
+    };
   }
 }
