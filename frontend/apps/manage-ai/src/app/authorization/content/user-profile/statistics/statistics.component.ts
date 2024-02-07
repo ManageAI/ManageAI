7646c45c23857dcd81b0c828ec9b68de3f85e1b9
patrykluszcz <patis00@wp.pl>
2024-02-07 17:47:16 +0100

    feat: add change detection strategy to components

--- frontend/apps/manage-ai/src/app/authorization/content/user-profile/statistics/statistics.component.ts
+++ frontend/apps/manage-ai/src/app/authorization/content/user-profile/statistics/statistics.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-statistics',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.scss'],
+  imports: [CommonModule],
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StatisticsComponent {}
--- frontend/apps/manage-ai/src/core/components/navbar/navbar.component.ts
+++ frontend/apps/manage-ai/src/core/components/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
+  imports: [CommonModule],
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarComponent {}
