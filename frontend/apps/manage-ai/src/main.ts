ada10f5fdd22980116fa6d81ca90e4dce6906764
patrykluszcz <patis00@wp.pl>
2024-02-17 16:15:58 +0100

    conf: add provideAngularSvgIcon to providers while bootstraping application

--- frontend/apps/manage-ai/src/main.ts
+++ frontend/apps/manage-ai/src/main.ts
@@ -4,15 +4,15 @@ import { appRoutes } from './app/app.routes';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from "@angular/common/http";
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideAngularSvgIcon } from 'angular-svg-icon';
 
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule),
-        provideAnimations(),
-        provideRouter(appRoutes),
-        provideHttpClient()
-    ]
-})
-  .catch((err) => console.error(err));
+  providers: [
+    importProvidersFrom(BrowserModule),
+    provideAngularSvgIcon(),
+    provideAnimations(),
+    provideRouter(appRoutes),
+    provideHttpClient(),
+  ],
+}).catch((err) => console.error(err));
