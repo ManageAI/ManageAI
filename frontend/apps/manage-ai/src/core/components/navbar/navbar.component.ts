b6d1662712ab64462be52eda54f94833d7c366c4
patrykluszcz <patis00@wp.pl>
2024-02-17 21:47:07 +0100

    feat: implement logic for navigation bar + rename classes

--- frontend/apps/manage-ai/src/core/components/navbar/navbar.component.ts
+++ frontend/apps/manage-ai/src/core/components/navbar/navbar.component.ts
@@ -1,12 +1,38 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SvgIconComponent } from 'angular-svg-icon';
+import { NavbarConstants } from '../../constants/navbar.constants';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
-  imports: [CommonModule],
+  imports: [CommonModule, SvgIconComponent],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit {
+  navbarLinks = NavbarConstants.navbarLinks;
+  selectedLink!: string;
+
+  constructor(private _router: Router) {}
+
+  public ngOnInit(): void {
+    this.selectedLink = this._checkRouting();
+  }
+
+  private _checkRouting(): string {
+    const url = this._router.url;
+    const route = this.navbarLinks.find(({ name }) => url.includes(name));
+
+    return route?.name ?? 'dashboard';
+  }
+
+  public selectLink(linkName: string): void {
+    this.selectedLink = linkName;
+
+    //TODO: it will require to implement more flexible method for other profiles like ceo and clients
+    this._router.navigateByUrl('user-profile/' + linkName);
+  }
+}
