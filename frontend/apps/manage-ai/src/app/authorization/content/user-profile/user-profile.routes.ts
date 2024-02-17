68c9ddb9d9210f166851a91a714a49496545ca8d
patrykluszcz <patis00@wp.pl>
2024-02-17 20:52:43 +0100

    feat: add routes to new created components in user profile routes file

--- frontend/apps/manage-ai/src/app/authorization/content/user-profile/user-profile.routes.ts
+++ frontend/apps/manage-ai/src/app/authorization/content/user-profile/user-profile.routes.ts
@@ -1,6 +1,9 @@
 import { Route } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { StatisticsComponent } from './statistics/statistics.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { MessagesComponent } from './messages/messages.component';
+import { SettingsComponent } from './settings/settings.component';
 
 export const userProfileRoutes: Route[] = [
   {
@@ -15,5 +18,17 @@ export const userProfileRoutes: Route[] = [
   {
     path: 'statistics',
     component: StatisticsComponent,
+  },
+  {
+    path: 'calendar',
+    component: CalendarComponent,
+  },
+  {
+    path: 'messages',
+    component: MessagesComponent,
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent,
   },
 ];
