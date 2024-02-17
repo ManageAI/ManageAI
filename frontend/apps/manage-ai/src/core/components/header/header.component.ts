92e958233ee063e8fab1cbd6dbbbc448070c8a5e
patrykluszcz <patis00@wp.pl>
2024-02-17 23:30:13 +0100

    feat: implement ui input search into header template

--- frontend/apps/manage-ai/src/core/components/header/header.component.ts
+++ frontend/apps/manage-ai/src/core/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InputSearchComponent } from '../../../../../..//libs/shared-components/src/lib/components/input-search/input-search.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, InputSearchComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
