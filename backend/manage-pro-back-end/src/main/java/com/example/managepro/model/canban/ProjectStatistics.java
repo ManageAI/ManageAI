fa47c58ed7536e6a227ca3aa19527bb67df0eca7
MaksymKokora <maksym.kokora@gmail.com>
2024-02-18 11:58:30 +0100

    fix: delete unnecessary No args constructor

--- backend/manage-pro-back-end/src/main/java/com/example/managepro/model/canban/ProjectStatistics.java
+++ backend/manage-pro-back-end/src/main/java/com/example/managepro/model/canban/ProjectStatistics.java
@@ -2,11 +2,9 @@ package com.example.managepro.model.canban;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
 public class ProjectStatistics {
