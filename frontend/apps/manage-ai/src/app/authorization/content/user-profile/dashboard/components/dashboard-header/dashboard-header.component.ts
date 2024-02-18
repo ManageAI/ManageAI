4ddb3a1b0f8618683081d1dcfcc3386943c493c5
patrykluszcz <patis00@wp.pl>
2024-02-18 12:58:01 +0100

    feat: create dashboard header component

---
+++ frontend/apps/manage-ai/src/app/authorization/content/user-profile/dashboard/components/dashboard-header/dashboard-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dashboard-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dashboard-header.component.html',
+  styleUrls: ['./dashboard-header.component.scss'],
+})
+export class DashboardHeaderComponent {}
