36d340cae3ef8d0a94b39a35007b9749a90ee0e8
patrykluszcz <patis00@wp.pl>
2024-02-18 12:58:21 +0100

    feat: create dashboard canban component

---
+++ frontend/apps/manage-ai/src/app/authorization/content/user-profile/dashboard/components/dashboard-canban/dashboard-canban.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dashboard-canban',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dashboard-canban.component.html',
+  styleUrls: ['./dashboard-canban.component.scss'],
+})
+export class DashboardCanbanComponent {}
