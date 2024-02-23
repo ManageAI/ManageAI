d170bedc69842542fd9ed8d49941606e6a3794e6
patrykluszcz <patis00@wp.pl>
2024-02-23 21:35:26 +0100

    feat: add polish and english languages and set default english as a default

--- frontend/apps/manage-ai/src/app/app.component.ts
+++ frontend/apps/manage-ai/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: true,
-    imports: [RouterOutlet],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [RouterOutlet],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['english', 'polish']);
+    translate.setDefaultLang('english');
+  }
+}
