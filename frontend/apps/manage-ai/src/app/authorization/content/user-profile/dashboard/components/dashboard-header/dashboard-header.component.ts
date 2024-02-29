51719a9929b074d52b9ca9ff54d99e88b0d01b63
patrykluszcz <patis00@wp.pl>
2024-03-01 00:35:51 +0100

    ref: replace add new task button in header of canban by reusable ui button

--- frontend/apps/manage-ai/src/app/authorization/content/user-profile/dashboard/components/dashboard-header/dashboard-header.component.ts
+++ frontend/apps/manage-ai/src/app/authorization/content/user-profile/dashboard/components/dashboard-header/dashboard-header.component.ts
@@ -2,13 +2,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SvgIconComponent } from 'angular-svg-icon';
+import { ButtonComponent } from '../../../../../../../../../../libs/ui-components/src/lib/components/button/button.component';
 
 @Component({
   selector: 'app-dashboard-header',
   standalone: true,
-  imports: [CommonModule, FormsModule, SvgIconComponent],
+  imports: [CommonModule, FormsModule, SvgIconComponent, ButtonComponent],
   templateUrl: './dashboard-header.component.html',
   styleUrls: ['./dashboard-header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardHeaderComponent {}
+export class DashboardHeaderComponent {
+  addIconSrc = 'assets/icons/add-icon.svg';
+
+  public openModal(): void {
+    console.log('clicked');
+  }
+}
