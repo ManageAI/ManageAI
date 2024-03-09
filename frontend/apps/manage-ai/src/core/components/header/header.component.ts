47f4ad76ac7424107eb099f8e1f12a67bbc89058
patrykluszcz <patis00@wp.pl>
2024-03-09 19:09:04 +0100

    feat: connect profile dropdown directive to profile photo container

--- frontend/apps/manage-ai/src/core/components/header/header.component.ts
+++ frontend/apps/manage-ai/src/core/components/header/header.component.ts
@@ -1,41 +1,15 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputSearchComponent } from '@frontend/ui-components';
-import { TranslateService } from '@ngx-translate/core';
-import { UserPreferencesService } from '../../services/user-preferences.service';
 import { SvgIconComponent } from 'angular-svg-icon';
+import { ProfileDropdownDirective } from '../profile-dropdown/profile-dropdown.directive';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [CommonModule, InputSearchComponent, SvgIconComponent],
+  imports: [CommonModule, InputSearchComponent, SvgIconComponent, ProfileDropdownDirective],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {
-  polishLanguageIcon = 'assets/icons/polish-language.svg';
-  englishLanguageIcon = 'assets/icons/english-language.svg';
-
-  isEnglishLanguageSet = true;
-  isDarkMode = false;
-
-  private _translateService = inject(TranslateService);
-  private _userPreferencesService = inject(UserPreferencesService);
-
-  public switchLanguage(): void {
-    this.isEnglishLanguageSet = !this.isEnglishLanguageSet;
-
-    if (this.isEnglishLanguageSet) {
-      this._translateService.use('english');
-    } else {
-      this._translateService.use('polish');
-    }
-  }
-
-  public setDarkMode(): void {
-    this.isDarkMode = !this.isDarkMode;
-
-    this._userPreferencesService.setDarkMode(this.isDarkMode);
-  }
-}
+export class HeaderComponent {}
