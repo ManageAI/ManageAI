337de798ce16aa52d78dcd2d77a1163f9df87b36
patrykluszcz <patis00@wp.pl>
2024-03-16 15:11:51 +0100

    feat: adapt logic of ui tag component to work with filter and dropdown list

--- frontend/libs/ui-components/src/lib/components/tag/tag.component.ts
+++ frontend/libs/ui-components/src/lib/components/tag/tag.component.ts
@@ -1,13 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SvgIconComponent } from 'angular-svg-icon';
+import { DisplayNamePipe } from '../../pipe/display-name.pipe';
+import { FilterProps } from '../../interfaces/filter-props.interface';
 
 @Component({
   selector: 'ui-tag',
-  standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SvgIconComponent, DisplayNamePipe],
   templateUrl: './tag.component.html',
   styleUrls: ['./tag.component.scss'],
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TagComponent {
-  @Input() content!: string;
+  @Input() item!: FilterProps;
+  @Input() itemContent!: string;
+
+  @Output() removeTag = new EventEmitter();
 }
