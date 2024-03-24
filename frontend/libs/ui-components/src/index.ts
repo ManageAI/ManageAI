583173f1684b72f50dd43cf1735350fd6db6fd44
patrykluszcz <patis00@wp.pl>
2024-03-24 22:27:00 +0100

    feat: create reusable modal component

--- frontend/libs/ui-components/src/index.ts
+++ frontend/libs/ui-components/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/components/input-search/input-search.component';
 export * from './lib/components/button/button.component';
 export * from './lib/components/tag/tag.component';
 export * from './lib/components/filter/filter.component';
+export * from './lib/components/modal/modal.component';
