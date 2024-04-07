2964966af9c57e9d1f171d6f84c62475df6c4309
patrykluszcz <patis00@wp.pl>
2024-04-07 16:50:50 +0200

    ref: use reusable task component instead of redundancy code

--- frontend/apps/manage-ai/src/app/authorization/content/user-profile/dashboard/components/dashboard-canban/dashboard-canban.component.ts
+++ frontend/apps/manage-ai/src/app/authorization/content/user-profile/dashboard/components/dashboard-canban/dashboard-canban.component.ts
@@ -8,21 +8,61 @@ import {
   CdkDropList,
 } from '@angular/cdk/drag-drop';
 import { TagComponent } from '@frontend/ui-components';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-dashboard-canban',
   standalone: true,
-  imports: [CommonModule, CdkDrag, CdkDropList, TagComponent],
+  imports: [CommonModule, CdkDrag, CdkDropList, TagComponent, TaskComponent],
   templateUrl: './dashboard-canban.component.html',
   styleUrls: ['./dashboard-canban.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardCanbanComponent {
-  todo = ['Get to work', 'Pick up groceries', 'Go home'];
-  inProgress = ['Pick up groceries'];
-  done = ['Get up', 'Brush teeth'];
+  todo = [
+    {
+      id: '1',
+      title: 'Get to work',
+      description: 'Get rubbish from the apartment and throw them into the bin',
+      tags: ['Chores', 'Food'],
+      timeLeft: '4 hours',
+      assignedUsersPhoto: ['assets/icons/person.svg'],
+      commentsAmount: '1',
+    },
+  ];
+  inProgress = [
+    {
+      id: '1',
+      title: 'Pick up groceries',
+      description: 'Get rubbish from the apartment and throw them into the bin',
+      tags: ['Chores', 'Food'],
+      timeLeft: '4 hours',
+      assignedUsersPhoto: ['assets/icons/person.svg'],
+      commentsAmount: '1',
+    },
+  ];
+  done = [
+    {
+      id: '1',
+      title: 'Get up',
+      description: 'Get rubbish from the apartment and throw them into the bin',
+      tags: ['Chores', 'Food'],
+      timeLeft: '4 hours',
+      assignedUsersPhoto: ['assets/icons/person.svg'],
+      commentsAmount: '1',
+    },
+    {
+      id: '2',
+      title: 'Brush teeth',
+      description: 'Get rubbish from the apartment and throw them into the bin',
+      tags: ['Chores', 'Food'],
+      timeLeft: '4 hours',
+      assignedUsersPhoto: ['assets/icons/person.svg', 'assets/icons/person.svg'],
+      commentsAmount: '1',
+    },
+  ];
 
-  drop(event: CdkDragDrop<string[]>) {
+  public drop(event: CdkDragDrop<any[]>): void {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
