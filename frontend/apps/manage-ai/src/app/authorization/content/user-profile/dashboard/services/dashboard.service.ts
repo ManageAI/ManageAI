9021b1019bb91fa373fdcdd4d5feb6ce7c664722
patrykluszcz <patis00@wp.pl>
2024-05-22 21:43:09 +0200

    feat: implement RxJS pipe and map operators to extract taskStates directly from the API response

--- frontend/apps/manage-ai/src/app/authorization/content/user-profile/dashboard/services/dashboard.service.ts
+++ frontend/apps/manage-ai/src/app/authorization/content/user-profile/dashboard/services/dashboard.service.ts
@@ -1,14 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { environment } from 'apps/manage-ai/src/environments/environment';
+import { Observable, map } from 'rxjs';
+import { Project } from '../interfaces/project.interface';
+import { TaskState } from '../interfaces/task-state.interface';
 
 @Injectable()
 export class DashboardService {
   private _url = environment.url;
-
   private _httpClient = inject(HttpClient);
 
-  public getTaskState() {
-    return this._httpClient.get(`${this._url}/api/v1/project/2`);
+  public getTaskState(): Observable<TaskState[]> {
+    return this._httpClient
+      .get<Project>(`${this._url}/api/v1/project/1`)
+      .pipe(map(({ taskStates }) => taskStates));
   }
 }
